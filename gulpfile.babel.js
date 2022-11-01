b0fb039d04f2cca886ab94878fbf2c37356a1636
Adam Pócs <adam.pocs@edreamsodigeo.com>
2022-11-01 22:18:53 +0100

    use one build directory only

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,9 +7,6 @@ import rimraf from "rimraf";
 import browser from 'browser-sync';
 import sass from 'gulp-dart-sass';
 import plugins from "gulp-load-plugins";
-import inlinesource from 'gulp-inline-source';
-import yargs from 'yargs';
-import jsonminify from 'gulp-jsonminify';
 import ts from 'gulp-typescript';
 import fileinclude from 'gulp-file-include';
 
@@ -25,21 +22,12 @@ const $ = plugins();
 
 const CONF = {
 	'sourceDir': 'src',
-	'buildDirDev': 'build/dev',
-	'buildDirProd': 'build/prod',
+	'buildDir': 'build',
 	'nameTemplate': 'index',
 	'nameScript': 'app',
 	'nameStyle': 'style',
 	'nameData': 'data'
 }
-
-const WATCH_PROD = !!(yargs.argv.watch);
-
-// ================================================
-// Variables
-// ================================================
-
-let buildDir;
 
 // ****************************************************************
 // TASKS
@@ -53,23 +41,13 @@ let buildDir;
 // Common
 // --------------------------------
 
-function setBuildDirToDev(cb) {
-	buildDir = CONF['buildDirDev'];
-	cb();
-}
-
-function setBuildDirToProd(cb) {
-	buildDir = CONF['buildDirProd'];
-	cb();
-}
-
 function clean(done) {
-	rimraf(buildDir, done);
+	rimraf(`${CONF["buildDir"]}`, done);
 }
 
 function templates() {
 	return gulp.src([`${CONF['sourceDir']}/*.html`])
-		.pipe(gulp.dest(buildDir));
+		.pipe(gulp.dest(`${CONF["buildDir"]}`));
 }
 
 function script() {
@@ -80,23 +58,23 @@ function script() {
 			outFile: `${CONF['nameScript']}.js`,
 			removeComments: true
 		}))
-		.pipe(gulp.dest(buildDir));
+		.pipe(gulp.dest(`${CONF["buildDir"]}`));
 }
 
 function style() {
 	return gulp.src(`${CONF["sourceDir"]}/${CONF['nameStyle']}.scss`)
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(buildDir));
+		.pipe(gulp.dest(`${CONF["buildDir"]}`));
 }
 
 function data() {
 	return gulp.src([`${CONF['sourceDir']}/${CONF['nameData']}.json`])
-		.pipe(gulp.dest(buildDir));
+		.pipe(gulp.dest(`${CONF["buildDir"]}`));
 }
 
 function serve(done) {
 	browser.init({
-		server: buildDir
+		server: `${CONF["buildDir"]}`
 	});
 	done();
 }
@@ -106,36 +84,14 @@ function serve(done) {
 // --------------------------------
 
 function includeFile() {
-	return gulp.src([`${buildDir}/${CONF['nameTemplate']}.html`])
+	return gulp.src([`${CONF["buildDir"]}/${CONF['nameTemplate']}.html`])
 		.pipe(fileinclude())
-		.pipe(gulp.dest(`${buildDir}`));
-}
-
-function minifyJs() {
-	return gulp.src([`${buildDir}/${CONF['nameTemplate']}.html`])
-		.pipe(inlinesource())
-		.pipe(gulp.dest(`${buildDir}`));
-}
-
-function minifyJson() {
-	return gulp.src([`${buildDir}/${CONF['nameData']}.json`])
-		.pipe(jsonminify())
-		.pipe(gulp.dest(buildDir));
+		.pipe(gulp.dest(`${CONF["buildDir"]}`));
 }
 
 function watchDev() {
 	gulp.watch(`${CONF['sourceDir']}/*.(html|scss|ts|json)`).on('all', gulp.series(
 		'build',
-		includeFile,
-		browser.reload
-	));
-}
-
-function watchProd() {
-	gulp.watch(`${CONF['sourceDir']}/*.*`).on('all', gulp.series(
-		'build',
-		minifyJs,
-		minifyJson,
 		includeFile,
 		browser.reload
 	));
@@ -157,32 +113,9 @@ gulp.task('build', gulp.series(
 // ================================================
 
 gulp.task('default', gulp.series(
-	setBuildDirToDev,
 	clean,
 	'build',
 	includeFile,
 	serve,
 	watchDev
 ));
-
-if(WATCH_PROD) {
-	gulp.task('prod', gulp.series(
-		setBuildDirToProd,
-		clean,
-		'build',
-		minifyJs,
-		minifyJson,
-		includeFile,
-		serve,
-		watchProd
-	));
-} else {
-	gulp.task('prod', gulp.series(
-		setBuildDirToProd,
-		clean,
-		'build',
-		minifyJs,
-		minifyJson,
-		includeFile
-	));
-}
